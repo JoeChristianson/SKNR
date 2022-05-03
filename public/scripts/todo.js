728221d89aa73cb55809db15ed0ce5fda4aea1b8
Joe Christianson <jcfargond@gmail.com>
2022-05-02 21:32:57 -0500

    added some css classes

--- public/scripts/todo.js
+++ public/scripts/todo.js
@@ -21,10 +21,12 @@ body.on('click','.to-do-btn',async (e)=>{
       console.log("in this")
       e.target.classList.add("done");
       e.target.dataset.isComplete=true;
+      e.target.parentElement.children[1].classList.add("done-text")
     }
     else{
       e.target.dataset.isComplete=false;
       e.target.classList.remove("done");
+      e.target.parentElement.children[1].classList.remove("done-text")
     }
 })
 
@@ -46,9 +48,17 @@ toDoForm.on('submit',async (e)=>{
 
 const addToDoToPage = (val,id)=>{
   const cont = $(".to-dos-content");
+  const wrapper = $("<div>");
+  wrapper.addClass("to-do-wrapper");
   const btn = $("<button>");
+  const toDoDiv = $("<div>")
   btn.attr({'data-to-do-id':id,'data-is-complete':false});
   btn.addClass("to-do-btn");
-  btn.text(val)
-  cont.append(btn)
+  toDoDiv.addClass("to-do");
+  toDoDiv.text(val)
+  toDoDiv.attr({'data-to-do-id':id,'data-is-complete':false});
+  wrapper.append(btn);
+  wrapper.append(toDoDiv)
+  cont.append(wrapper)
+  newToDo.val("")
 }
