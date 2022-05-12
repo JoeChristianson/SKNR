cb18dac750a2549e8a3b137e4d8f4e71f4368e0d
Joe Christianson <jcfargond@gmail.com>
2022-05-12 13:41:57 -0500

    made nav work

--- public/scripts/footer.js
+++ public/scripts/footer.js
@@ -1,5 +1,11 @@
 const footer = $("footer");
 
-footer.on("click","span",()=>{
-    console.log("clicked")
+footer.on("click","span",(e)=>{
+    const modId = e.target.dataset.mod;
+    console.log(modId);
+    const mods = document.querySelectorAll(".mod");
+    mods.forEach(mod=>{
+        mod.classList.add("mhide")
+    })
+    document.getElementById(modId).classList.remove("mhide")
 })
